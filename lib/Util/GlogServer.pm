dc4f3eb322f39d0ef41a44b078a566442191a2d4
Gordon Marler <gmarler@gmarler.com>
2015-01-13 20:18:18 -0500

    Handle cleanup of client that is no longer sending

--- lib/Util/GlogServer.pm
+++ lib/Util/GlogServer.pm
@@ -6,15 +6,26 @@ use feature qw(say);
 use Moose;
 with 'MooseX::Getopt';
 use IO::Async::Loop;
+use IO::File;
+use Data::Dumper;
 
 use namespace::autoclean;
 
 
-has 'sockpath'    => ( is => 'rw', isa => 'Str',
-                       default => "/tmp/glogserver2.sock" );
-has '_loop'       => (is => 'rw', isa => 'IO::Async::Loop',
-                      lazy => 1, builder => '_build_loop',
-                     );
+has 'sockpath'      => ( is => 'rw', isa => 'Str',
+                         default => "/tmp/glogserver2.sock" );
+
+has '_loop'         => (is => 'rw', isa => 'IO::Async::Loop',
+                        lazy => 1, builder => '_build_loop',
+                       );
+
+has '_log_table'    => (is => 'rw', isa => 'HashRef',
+                        default => sub { return {}; },
+                       );
+
+has '_stream_table' => (is => 'rw', isa => 'HashRef',
+                        default => sub { return {}; },
+                       );
 
 sub _build_loop {
   my $self = shift;
@@ -48,6 +59,8 @@ sub run {
     on_stream => sub {
       my ( $stream ) = @_;
 
+      my $log_table = $self->_log_table;
+      my $logdata = {};
       # PROTOCOL:
       #
       # CLIENT: Send desired log pathname
@@ -65,10 +78,32 @@ sub run {
       #
       $stream->push_on_read(
         sub {
-          my ($self, $buffref, $eof) = @_;
+          my ($stream_obj, $buffref, $eof) = @_;
+
+          my $stream_table = $self->_stream_table;
 
           if ( $$buffref =~ s/^(.*)\n// ) {
             say "Client requested log file: $1";
+            my $logfile = $1;
+            # Reject if already in list of files we're managing
+            if ( exists( $log_table->{$logfile} ) ) {
+              say "Log file $logfile already being written to!\n";
+              # TODO: Send reject message and close message
+              $stream->close;
+            } else {
+              if (my $fh = IO::File->new($logfile,">")) {
+                # TODO: Send Acceptance message
+                $logdata->{fh} = $fh;
+                $log_table->{$logfile} = $logdata;
+                $stream_table->{$stream_obj} = $logfile;
+                $self->_log_table($log_table);
+                say Data::Dumper->Dump([ $log_table ]);
+              } else {
+                # TODO: Send reject message and close connection
+                say "Unable to open file $logfile";
+                $stream->close;
+              }
+            }
           }
 
           if ($eof) {
@@ -81,11 +116,22 @@ sub run {
 
       $stream->configure(
         on_read => sub {
-          my ( $self, $buffref, $eof ) = @_;
+          my ( $stream_obj, $buffref, $eof ) = @_;
 
           if ($eof) {
-            # For the moment, do nothing
             # TODO: flush the output log and clean up
+            say "Cleaning up after: $stream_obj";
+            my $stream_table = $self->_stream_table;
+            my $log_table    = $self->_log_table;
+            my $logfile      = $stream_table->{$stream_obj};
+
+            delete $stream_table->{$stream_obj};
+            delete $log_table->{$logfile};
+
+            $self->_log_table($log_table);
+            $self->_stream_table($stream_table);
+
+            $stream_obj->close;
           } elsif ( $$buffref =~ s/^(.*)\n// ) {
             say "$1";
             return 1;
