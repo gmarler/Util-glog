c70de78bc8c328ab78358061654b6674c0ed8b56
Gordon Marler <gmarler@gmarler.com>
2015-05-12 22:37:45 -0400

    Able to start server child process and confirm it's up.

--- t/lib/TF/Util/GlogClient.pm
+++ t/lib/TF/Util/GlogClient.pm
@@ -2,11 +2,19 @@ package TF::Util::GlogClient;
 
 use Log::Log4perl     qw();
 use Util::GlogServer  qw();
+use Child             qw();
 
 use Test::Class::Moose;
 with 'Test::Class::Moose::Role::AutoUse';
 
-has 'gserver' => ( is => 'rw', isa => 'Util::GlogServer' );
+# The GlogServer instance
+has 'gserver'  => ( is => 'rw', isa => 'Util::GlogServer', );
+# The test Unix Socket path the server and client will use
+has 'sockpath' => (is => 'rw', isa => 'Str', default => '/tmp/gclient_tdd.sock', );
+
+has 'serverchildren' => (is => 'rw', isa => 'ArrayRef', default => sub { return []; }, 
+
+has 'clientchildren' => (is => 'rw', isa => 'ArrayRef', default => sub { return []; },
 
 sub test_startup {
   my ($test) = shift;
@@ -32,8 +40,23 @@ sub test_startup {
   Log::Log4perl::init( \$conf );
 
   # Create a GlogServer instance to test against, sitting on a test socket path
-  my $gserver = Util::GlogServer->new();
-  $test->gserver( $gserver );
+  #my $gserver = Util::GlogServer->new( sockpath => $test->sockpath );
+  #$test->gserver( $gserver );
+
+  # fork() off the test GlogServer
+  my $child =
+    Child->new(
+      sub {
+        my ($parent) = @_;
+        my $gserver = Util::GlogServer->new( sockpath => $test->sockpath );
+        my $future = $gserver->run();
+
+        $future->loop->run();
+      }
+    );
+  my $proc = $child->start();
+  diag( "PROC is: " . $proc );
+  push @{$test->serverchildren}, $proc;
 }
 
 sub test_shutdown {
@@ -41,7 +64,8 @@ sub test_shutdown {
   # more teardown
   $test->next::method;
   # Shut Down the GlogServer
-  # $test->gserver->shutdown();
+  my $proc = shift @{$test->serverchildren};
+  $proc->kill(1);
 }
 
 # TODOs:
@@ -54,7 +78,10 @@ sub test_shutdown {
 sub test_server_is_ok {
   my $test = shift;
 
-  isa_ok($test->gserver, 'Util::GlogServer');
+  #isa_ok($test->gserver, 'Util::GlogServer');
+  # It'll take a second or two before the socket is created
+  sleep(1);
+  is( -S $test->sockpath, 1, "Test Unix socket " . $test->sockpath . " exists" );
 }
 
 1;
